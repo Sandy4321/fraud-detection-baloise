97d3aefb6d36d27f8f515de4b205608f22066d38
svincent <svincent@student.ethz.ch>
2016-09-16 13:45:31 +0200

    Support vector machines

---
+++ ML-modules/learn.py
@@ -0,0 +1,41 @@
+import numpy as np
+# import sklearn.cross_validation as skcv
+import sklearn.svm as sksvm
+import sklearn.grid_search as skgs
+# import data
+from sklearn import linear_model
+
+class Learn:
+
+    # This method wil initialize the model(s) and train them using the data interface
+    # The data is expected to be a numpy array of the shape: (n_samples, n_features)
+    def init(self):
+        return NotImplemented
+
+    def predict(self, new_sample):
+        return NotImplemented
+
+def svcSIGMOIDGridSearch(X, y, Test):
+    param_grid = [{'C': np.logspace(-3.20,10), 'gamma': np.logspace(-5,3,20), 'kernel': ['sigmoid']}]
+    grid_search = skgs.GridSearchCV(sksvm.SVC(), param_grid, cv=5)
+    grid_search.fit(X,y)
+    print 'Best Score of Grid Search: ' + str(grid_search.best_score_)
+    print 'Best Params of Grid Search: ' + str(grid_search.best_params_)
+
+def svcPOLYGridSearch(X, y, Test):
+    param_grid = [{'degree': np.linspace(1,10,10),'C': np.logspace(-3.20,10), 'gamma': np.logspace(-5,3,20), 'kernel': ['poly']}]
+    grid_search = skgs.GridSearchCV(sksvm.SVC(), param_grid, cv=5)
+    grid_search.fit(X,y)
+    print 'Best Score of Grid Search: ' + str(grid_search.best_score_)
+    print 'Best Params of Grid Search: ' + str(grid_search.best_params_)
+
+def svcRBFGridsearch(X, y, Test):
+    param_grid = [{'C': np.logspace(-1,20,10), 'gamma': np.logspace(-5,3,20), 'probability':[True, False], 'kernel': ['rbf']}]
+    grid_search = skgs.GridSearchCV(sksvm.SVC(), param_grid, cv=5)
+    grid_search.fit(X,y)
+    print 'Best Score of Grid Search: ' + str(grid_search.best_score_)
+    print 'Best Params of Grid Search: ' + str(grid_search.best_params_)
+
+if __name__ == '__main__':
+    print np.linspace(1,5,5)
+        
