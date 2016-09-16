7412205a9d759b049687b72fc7b0360a051cc6dd
svincent <svincent@student.ethz.ch>
2016-09-16 17:34:09 +0200

    hh

---
+++ rules.py
@@ -0,0 +1,17 @@
+#import data
+class Rules:
+    def isFraud(damage):
+        # Hagel Regel
+        if damage.data['VERSARTGRP'] == 'MF Kasko' &&
+           damage.data['VERSART'] == 'Teilkasko' &&
+           damage.data['SDART'] == 'Elementar' &&
+           damage.data['SDURS'] == 'Hagel':
+            # TODO: if hagel return true, else false
+            # damage.data['SDERDAT']
+        if damage.data['VERSARTGRP'] == 'Wertsachen' &&
+           damage.data['VERSART'] == 'einfacher Diebstahl' &&
+           damage.data['SDART'] == 'Kombiversicherung Privathaushalt' &&
+           damage.data['SDURS'] == 'einfacher Diebstahl' &&:
+        
+            
+        
