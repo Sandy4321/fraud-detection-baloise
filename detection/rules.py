cf6eca6bd6e48281b0806082239ebb05dd5d3e8d
svincent <svincent@student.ethz.ch>
2016-09-16 17:43:06 +0200

    self added

--- detection/rules.py
+++ detection/rules.py
@@ -1,6 +1,8 @@
 #import data
+# return tuple (int, String)
 class RuleDetection:
-    def isFraud(damage):
+    def isFraud(self, damage):
+        
         # Hagel Regel
         if damage.data['VERSARTGRP'] == 'MF Kasko' &&
            damage.data['VERSART'] == 'Teilkasko' &&
@@ -8,6 +10,8 @@ class RuleDetection:
            damage.data['SDURS'] == 'Hagel':
             # TODO: if hagel return true, else false
             # damage.data['SDERDAT']
+        
+        # Iphone
         if damage.data['VERSARTGRP'] == 'Wertsachen' &&
            damage.data['VERSART'] == 'einfacher Diebstahl' &&
            damage.data['SDART'] == 'Kombiversicherung Privathaushalt' &&
