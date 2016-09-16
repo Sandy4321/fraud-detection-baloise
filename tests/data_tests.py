c84b2ae6e887924e9867404fcb31aed4fca42d86
Mohammed Ajil <muy@ajil.ch>
2016-09-16 20:04:47 +0200

    new data interface

--- tests/data_tests.py
+++ tests/data_tests.py
@@ -1,5 +1,6 @@
-import data.data_access as data
+from data.data_access_local import LocalData as Data
+#from data.data_access_db import DbData as Data
 
-data_var = data.Data()
-data_var.getBatchFromDb()
+data_var = Data()
+data_var.getBatch()
 data_var.printCurrentBatch()
--- webserver/server.py
+++ webserver/server.py
@@ -1,16 +1,32 @@
 import BaseHTTPServer
 from data.data_model import DamageCase
+#from detection.rules import RuleDetection
 
 class DamageHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_POST(self):
         content_len = int(self.headers.getheader('content-length', -1))
         if content_len < 0:
             self.send_error(500, "Content length could not be read")
-        else:
+        elif self.path == "/newdamagecase":
             form_data = self.rfile.read(content_len)
+
+            # Generate DamageCase object
             damage_case = DamageCase()
             damage_case.generateFromFormData(form_data)
-            
+
+            self.response_default(form_data)
+            # Detect if some rule applies
+            #rule_detector = RuleDetection()
+            # (fraud, reason) = rule_detector.isFraud(damage_case)
+            # if fraud:
+
+            # else:
+        elif self.path == "/":
+            self.do_GET()
+        else:
+            self.send_error(404, 'File Not Found: %s' % self.path)
+
+
 
 
     def do_GET(self):
@@ -23,3 +39,9 @@ class DamageHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             index_file.close()
         else:
             self.send_error(404, 'File Not Found: %s' % self.path)
+
+    def response_default(self, data):
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html')
+        self.end_headers()
+        self.wfile.write(data)
