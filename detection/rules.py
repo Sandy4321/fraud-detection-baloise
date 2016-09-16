c1410e74cbf503c1165bf4dd32a1d58d108320f4
svincent <svincent@student.ethz.ch>
2016-09-16 20:34:41 +0200

    weather and ihpone release rules

--- detection/rules.py
+++ detection/rules.py
@@ -1,23 +1,37 @@
 #import data
+from datetime import datetime
+
 # return tuple (int, String)
 class RuleDetection:
+
+    # Powered by Forecast
+    # key: e0ab1d9dd47415f06c4f182a81d31a13
+    def hagel(time, place):
+        return True
+
     def isFraud(self, damage):
-        
         # Hagel Regel
-        if damage.data['VERSARTGRP'] == 'MF Kasko' &&
-           damage.data['VERSART'] == 'Teilkasko' &&
-           damage.data['SDART'] == 'Elementar' &&
+        if damage.data['VERSARTGRP'] == 'MF Kasko' and \
+           damage.data['VERSART'] == 'Teilkasko' and \
+           damage.data['SDART'] == 'Elementar' and \
            damage.data['SDURS'] == 'Hagel':
-            # TODO: if hagel return true, else false
-            # damage.data['SDERDAT'] and SDERORT
-        
+
+            return hagel(damage.data['SDERDAT'], damage.data['SDERORT'])
+
         # Iphone
-        if damage.data['VERSARTGRP'] == 'Wertsachen' &&
-           damage.data['VERSART'] == 'einfacher Diebstahl' &&
-           damage.data['SDART'] == 'Kombiversicherung Privathaushalt' &&
-           damage.data['SDURS'] == 'einfacher Diebstahl' &&:
-            # TODO: if close to iphonerealese
-            # damage.data['SDERDAT']
+        if damage.data['VERSARTGRP'] == 'Wertsachen' and \
+           damage.data['VERSART'] == 'einfacher Diebstahl' and \
+           damage.data['SDART'] == 'Kombiversicherung Privathaushalt' and \
+           damage.data['SDURS'] == 'einfacher Diebstahl':
+
+            date_obj = datetime.striptime(damage.data['SDERDAT'],'%Y-%m-%d')
+            iphonerelease = datetime.striptime('2016-09-16', '%Y-%m-%d')
+            delta = date_obj - iphonerelease
+            if delta <= 10:
+                return True
+            else:
+                return False
 
         # Deckungseinschluss
-        if damage.data[''] ==
+        if damage.dat
+a[''] ==
