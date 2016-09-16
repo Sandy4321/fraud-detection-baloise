d8f5a358721eb9e82ebaa1e3092654d255551f78
Mohammed Ajil <muy@ajil.ch>
2016-09-16 22:49:44 +0200

    url_handlers and data

--- webserver/url_handlers.py
+++ webserver/url_handlers.py
@@ -1,10 +1,10 @@
-from data.data_models import *
-#from detection.rules import RuleDetection
+from string import split
+from detection.rules import RuleDetection
 #from detection.learn import LearnDetection as LearnDetection
 from detection.fake_learn import FakeLearn as LearnDetection
 
-from data.data_access import DbData as Data
-#from data.data_access import LocalData as Data
+#from data.data_access import DbData as Data
+from data.data_access import LocalData as Data
 
 #Handle "/"
 def handle_root(request, content_len=0, type="GET"):
@@ -18,28 +18,33 @@ def handle_root(request, content_len=0, type="GET"):
 
 # Handle "/newdamagecase"
 def handle_newdamagecase(request, content_len=0, type="GET"):
-    # Generate a new damage case
-    form_data = request.rfile.read(content_len)
+    if type == "POST":
+        # Generate a new damage case
+        form_data = request.rfile.read(content_len)
+        parsed_data = read_form_data(form_data)
+        damage_no = int(parsed_data["damage_no"])
+        parsed_data.pop("damage_no")
 
-    # Generate DamageCase object
-    damage_case = DamageCase()
-    damage_case.generateFromFormData(form_data)
+        rule_detector = RuleDetection()
+        (parsed_data["rule_fraud"], parsed_data["rule_reason"]) = rule_detector.isFraud(damage_no)
 
-    request.DBG_response_default(form_data)
-    # Detect if some rule applies
-    # rule_detector = RuleDetection()
-    # (fraud, reason) = rule_detector.isFraud(damage_case)
-    # if fraud:
+        ml_detector = LearnDetection()
+        (parsed_data["ml_fraud"], parsed_data["ml_prob"]) = ml_detector.isFraud(damage_no)
 
-    # Detect if ML algo tells us something
-    # ml_detector = LearnDetector()
-    # (fraud, prob) = ml_detector.isFraud(damage_case)
+        Data.damage_cases[damage_no] = parsed_data
+
+        #TODO: Return to Person Page
+    else:
+        #TODO: Return Form to input data
+        pass
     return NotImplemented
 
 
 # Handle "/enterpono"
 def handle_enterpono(request, content_len=0, type="GET"):
-    return NotImplemented
+    if type == "POST":
+        form_data = request.rfile.read(content_len)
+        parsed_data = read_form_data(form_data)
 
 
 # Handle "/personinfo"
@@ -50,4 +55,12 @@ def handle_personinfo(request, content_len=0, type="GET"):
 # Handle all other cases
 def handle_404(request):
     request.send_error(404, 'File Not Found: %s' % request.path)
+
+def read_form_data(data):
+    features = split(data, "&")
+    dict = {}
+    for feature in features:
+        key_and_value = split(feature, "=")
+        dict[key_and_value[0]] = key_and_value[1]
+    return dict
 
