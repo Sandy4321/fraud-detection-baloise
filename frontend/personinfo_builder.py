f36141e586b5ef87ff8b9e8dda44a1f89862af4a
Mohammed Ajil <muy@ajil.ch>
2016-09-17 11:19:58 +0200

    personinfobuilder

--- frontend/personinfo_builder.py
+++ frontend/personinfo_builder.py
@@ -10,10 +10,10 @@ class PersonInfoBuilder():
                             "</tr>"
 
     person_table_rows = "<tr>" \
-                        "<td>{name}</td>" \
-                        "<td>{birthday}</td>" \
-                        "<td>{police}</td>" \
-                        "<td>{address}</td>" \
+                        "<td>{{name}}</td>" \
+                        "<td>{{birthday}}</td>" \
+                        "<td>{{police}}</td>" \
+                        "<td>{{address}}</td>" \
                         "</tr>"
 
     damage_table_headers = "<tr>" \
@@ -28,14 +28,14 @@ class PersonInfoBuilder():
                            "</tr>"
 
     damage_table_rows_format = "<tr>" \
-                               "<td>{damage_no}</td>" \
-                               "<td>{damage_group}</td>" \
-                               "<td>{damage_type}</td>" \
-                               "<td>{damage_date}</td>" \
-                               "<td>{damage_kind}</td>" \
-                               "<td>{damage_reason}</td>" \
-                               "<td bgcolor = \"{rule_color}\">{rule_reason}</td>" \
-                               "<td bgcolor = \"{ml_color}\">{ml_prob}</td>" \
+                               "<td>{{damage_no}}</td>" \
+                               "<td>{{damage_group}}</td>" \
+                               "<td>{{damage_type}}</td>" \
+                               "<td>{{damage_date}}</td>" \
+                               "<td>{{damage_kind}}</td>" \
+                               "<td>{{damage_reason}}</td>" \
+                               "<td bgcolor = \"{{rule_color}}\">{{rule_reason}}</td>" \
+                               "<td bgcolor = \"{{ml_color}}\">{{ml_prob}}</td>" \
                                "</tr>"
 
     damage_table_rows = ""
@@ -54,10 +54,13 @@ class PersonInfoBuilder():
     html_footer = ""
 
     def buildPersonInfo(self, police):
-        self.person_table_rows = self.person_table_rows.format(Data.customers[police])
-        damages = Data.customers[police]["damage_cases"]
+        data = Data()
+        person_data = data.customers[int(police)]
+        print (person_data["name"])
+        self.person_table_rows = self.person_table_rows.format(person_data)
+        damages = data.customers[int(police)]["damage_cases"]
         for damage in damages:
-            damage_case = Data.damage_cases[damage]
+            damage_case = data.damage_cases[damage]
             if damage_case["ml_fraud"]:
                 damage_case["ml_color"] = "#FF0000"
             else:
