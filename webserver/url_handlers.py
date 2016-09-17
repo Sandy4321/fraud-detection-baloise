84b544d2b9fbb7b18fc4eb8b5e7a2cc63f60f2d0
Mohammed Ajil <muy@ajil.ch>
2016-09-17 10:31:37 +0200

    resolve todos, cleanup

--- webserver/url_handlers.py
+++ webserver/url_handlers.py
@@ -9,7 +9,6 @@ from data.data_access import LocalData as Data
 
 #Handle "/"
 def handle_root(request, content_len=0, type="GET"):
-    #TODO: We could remove that
     index_file = open("../frontend/index.html")
     request.send_response(200)
     request.send_header('Content-Type', 'text/html')
@@ -39,13 +38,11 @@ def handle_newdamagecase(request, content_len=0, type="GET"):
 
         Data.damage_cases[damage_no] = parsed_data
 
-        #TODO: Return to Person Page
         #Again easiest is just to redirect to localhost/personinfo/pono
         request.send_response(301)
         request.send_header('Location', 'http://localhost:5000/personinfo/' + str(parsed_data["police"]))
         request.end_headers()
     else:
-        #TODO: Return Form to input data
         html_file = open("../frontend/newdamagecase.html")
         request.send_response(200)
         request.send_header('Content-Type', 'text/html')
@@ -56,19 +53,19 @@ def handle_newdamagecase(request, content_len=0, type="GET"):
 
 # Handle "/enterpono"
 def handle_enterpono(request, content_len=0, type="GET"):
-    #TODO: Check if URL makes sense, else call handle_404()
+    police = request.path.rsplit('/', 1)[-1]
+    if police not in Data.customers:
+        handle_404(request)
     if type == "POST":
         form_data = request.rfile.read(content_len)
         parsed_data = read_form_data(form_data)
 
-        #TODO: Return to correct person page
         #Just read pono from parsed_data and then call handle_personinfo
         #Even simpler would be just redirect to personinfo/pono
         request.send_response(301)
         request.send_header('Location', 'http://localhost:5000/personinfo/' + str(parsed_data["police"]))
         request.end_headers()
     else:
-        #TODO: Return Form to input PoNo (easy)
         html_file = open("../frontend/enterpono.html")
         request.send_response(200)
         request.send_header('Content-Type', 'text/html')
@@ -79,10 +76,8 @@ def handle_enterpono(request, content_len=0, type="GET"):
 # Handle "/personinfo"
 def handle_personinfo(request, content_len=0, type="GET"):
     if type == "POST":
-        #TODO: At the moment nothing
         pass
     else:
-        # TODO: Gather PersonInfo, Approved Damages, Disapproved Damages, Open Damages
         # PoNo should be in URL
         # Build table with function and inject to html file maybe
         # To make it easy create two files: top.html and bottom.html
